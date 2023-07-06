d6fa78c87eee01e38e36eec1ad49bed76d7fdfc3
Ali Al-Zuhairi <zohairi@live.com>
2023-07-06 05:59:20 +0300

    making the reading time dynamic

--- src/blogs/ReadingTimeEstimator.js
+++ src/blogs/ReadingTimeEstimator.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-const ReadingTimeEstimator = ({ content }) => {
+const ReadingTimeEstimator = ({ filePath }) => {
   const [readingTime, setReadingTime] = useState(0);
 
   useEffect(() => {
-    const wordsPerMinute = 250; 
-    const words = content.trim().split(/\s+/).length;
-    const time = Math.ceil(words / wordsPerMinute);
-    setReadingTime(time);
-  }, [content]);
+    const fetchContent = async () => {
+      try {
+        const response = await fetch(filePath);
+        const content = await response.text();
+        const wordsPerMinute = 250;
+        const words = content.trim().split(/\s+/).length;
+        const time = Math.ceil(words / wordsPerMinute);
+        setReadingTime(time);
+      } catch (error) {
+        console.error('Error fetching content:', error);
+      }
+    };
+
+    fetchContent();
+  }, [filePath]);
 
   return (
     <div>
-      <p className='blog-author-position text-secondary'>Estimated reading time: {readingTime} minute{readingTime === 1 ? '' : 's'}</p>
+      <p className='blog-author-position text-secondary'>
+        Estimated reading time: {readingTime} minute{readingTime === 1 ? '' : 's'}
+      </p>
     </div>
   );
 };
